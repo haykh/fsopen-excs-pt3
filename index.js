416878423490894d655ac5a6b58396b02e5e7c60
haykh <haykh.mfs+github@gmail.com>
2022-08-16 17:41:34 -0400

    🎨 3.1-3.6 done

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345"
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122"
+  }
+]
+
+app.get('/info', (_req, res) => {
+  res.send(`<p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date()}</p>`)
+})
+
+app.get('/api/persons', (_req, res) => {
+  res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const newID = Math.floor(Math.random() * 1000000);
+  if (!(req.body.name && req.body.number)) {
+    return res.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+  if (persons.find(p => p.name === req.body.name)) {
+    return res.status(400).json({
+      error: 'name exists in phonebook'
+    })
+  }
+  const newPerson = {
+    id: newID,
+    name: req.body.name,
+    number: req.body.number
+  }
+  persons = persons.concat(newPerson)
+  res.json(newPerson)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
