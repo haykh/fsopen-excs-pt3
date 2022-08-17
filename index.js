97f1bc9f694949b97b6f8dc5caf158c8cd6b53e3
haykh <haykh.mfs+github@gmail.com>
2022-08-16 20:11:28 -0400

    deploy

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ app.put('/api/persons/:id', (req, res) => {
   res.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
